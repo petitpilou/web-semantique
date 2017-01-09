6f222827ff2533dbe2aadd20fab74f5eb316b73c
Adrien MAGNIN <adrien.magnin@etu.unice.fr>
2017-01-09 10:39:55 +0100

    Ajout des modules nodejs

--- lpsil-web/server.js
+++ lpsil-web/server.js
@@ -1,8 +1,53 @@
 var express = require('express');
 var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
 var logger = require('log4js').getLogger('Server');
 var bodyParser = require('body-parser');
 var app = express();
+
+/* MySql Server */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: '<nom de vôtre base de données>'
+});
+connection.connect();
+connection.query('SELECT * from < table name >', function (err, rows, fields) {
+    if (!err)
+        logger.info('Le résultat de la requête: ', rows);
+    else
+        logger.error(err);
+});
+connection.end();
+
+/* Pool */
+var pool =  mysql.createPool({
+    connectionLimit : 100, //important
+    host : 'localhost',
+    user : '< MySQL username >',
+    password: '< MySQL password >',
+    database: '<nom de vôtre base de données>'
+});
+pool.getConnection(function(err,connection){
+    if (err) {
+        connection.release();
+        res.json({"code" : 100, "status" : "Erreur de connexion à la DB"});
+        return;
+    }
+    logger.info('connecté en tant que ' + connection.threadId);
+    connection.query("select * from user",function(err,rows){
+        connection.release();
+        if(!err) {
+            res.json(rows);
+        }
+    });
+    connection.on('error', function(err) {
+        res.json({"code" : 100, "status" : "Erreur de connexion à la DB"});
+        return;
+    });
+});
 
 // config
 app.set('view engine', 'ejs');
@@ -10,11 +55,9 @@ app.set('views', __dirname + '/views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('combined')); // Active le middleware de logging
-
 app.use(express.static(__dirname + '/public')); // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
 
-logger.info('server start');
-
+/* On affiche le formulaire d'enregistrement */
 app.get('/', function(req, res){
     res.redirect('/login');
 });
@@ -23,7 +66,20 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
+app.post('/login', function (req, res) {
+    // TODO vérifier si l'utilisateur existe
+});
+
+app.get('/register', function (req, res) {
+    // TODO ajouter un nouveau utilisateur
+});
+
+/* On affiche le profile  */
+app.get('/profile', function (req, res) {
+    // TODO
+    // On redirige vers la login si l'utilisateur n'a pas été authentifier
+    // Afficher le button logout
+});
+
+logger.info('server start');
 app.listen(1313);
-
-
-
